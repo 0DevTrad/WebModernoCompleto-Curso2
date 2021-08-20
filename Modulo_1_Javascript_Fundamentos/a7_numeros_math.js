d3d9eb0cc22ad228304ad6fe10cfa83f839bdf2e
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 01:26:35 -0300

    Numeros Math

---
+++ Modulo_1_Javascript_Fundamentos/a7_numeros_math.js
@@ -0,0 +1,16 @@
+// Calculando a área
+
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);
+
+console.log(area);
+console.log(typeof Math); //object
+
+Math.PI; // Constante PI 3,1415...
+Math.pow; // Calcula a potência (x ^ y)
+
+/*
+    Math é um objeto com vários métodos que podem
+    ser úteis na hora de realizar operações matemáticas,
+    evitando que sejam feitas de formas literais.
+*/
