f292fb73ef8fb805bf5e041d2fc9fef7f1384499
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 03:18:26 -0300

    Aula Objeto

---
+++ Modulo_1_Javascript_Fundamentos/a12_objeto.js
@@ -0,0 +1,26 @@
+// Criando um Objeto
+const prod1 = {};
+prod1.nome = "Celular Ultra Mega"; // Adicionando chave e valor
+prod1.preco = 4998.9;
+
+// Outra forma de adicionar chave e valor
+prod1["Desconto Legal"] = 0.4; // Evitar criar atributos com espaço
+
+console.log(prod1);
+
+// Criando objetos de forma literal
+const prod2 = {
+  nome: "Camisa Polo",
+  preco: 79.9,
+};
+
+console.log(prod2);
+
+/* 
+    Objeto x JSON
+    
+    Objeto é uma coleção de chaves e valor,
+    enquanto JSON é um formato textual.
+
+    Atributo = Chave 
+*/
