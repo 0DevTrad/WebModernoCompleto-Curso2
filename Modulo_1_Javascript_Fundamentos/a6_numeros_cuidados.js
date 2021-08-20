5fdbd96de6c694659654019f684ec36a22e4fab3
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 02:10:36 -0300

    atualizando aula 6

--- Modulo_1_Javascript_Fundamentos/a6_numeros_cuidados.js
+++ Modulo_1_Javascript_Fundamentos/a6_numeros_cuidados.js
@@ -4,6 +4,8 @@ console.log(7 / 0); // Infinity
 
 console.log("10" / 2); // O js entende que "10" pode ser convertido em número e executa a operação.
 
+console.log("5" + 2); // O '+' concatena, por isso aqui o resultado é 52 e não 7
+
 console.log("Show!" * 2); // NaN (Not a Number)
 
 console.log(0.1 + 0.7); // 0.7999999999999999 // Impreciso com operaçãoes com ponto flutuante
