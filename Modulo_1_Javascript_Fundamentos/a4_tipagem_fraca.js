05daec06abd0b97bc940ddd85583d898d2513e4c
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 00:40:06 -0300

    tipagem js

---
+++ Modulo_1_Javascript_Fundamentos/a4_tipagem_fraca.js
@@ -0,0 +1,13 @@
+/*
+    JS possui tipagem fraca ou dinâmica, o que permite 
+    que as variáveis podem ter o seu tipo alterado.
+*/
+
+let coisa = "Legal";
+console.log(typeof coisa); // string
+
+coisa = 240;
+console.log(typeof coisa); // number
+
+coisa = true;
+console.log(typeof coisa); // boolean
