67ffcd577d686fefab3d57202400221b2450adca
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 01:15:35 -0300

    cuidados com numeros

---
+++ Modulo_1_Javascript_Fundamentos/a6_numeros_cuidados.js
@@ -0,0 +1,13 @@
+// Alguns cuidados úteis quando for mexer com números.
+
+console.log(7 / 0); // Infinity
+
+console.log("10" / 2); // O js entende que "10" pode ser convertido em número e executa a operação.
+
+console.log("Show!" * 2); // NaN (Not a Number)
+
+console.log(0.1 + 0.7); // 0.7999999999999999 // Impreciso com operaçãoes com ponto flutuante
+
+let a = 10;
+console.log(a.toString()); // 10 (string)
+//console.log(10.toString()) // É possível usando uma variável mas não um literal
