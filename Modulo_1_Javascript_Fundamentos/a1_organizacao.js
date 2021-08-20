4513a35e18f01b939a59f4e811f48ad54144b615
Yago <yagocarvalho.dev@gmail.com>
2021-08-19 23:07:08 -0300

    organizacao js

---
+++ Modulo_1_Javascript_Fundamentos/a1_organizacao.js
@@ -0,0 +1,26 @@
+/*
+    Códigos JavaScrip são organizados em:
+
+    1. Sentença de Código
+    2. Blocos de código
+*/
+
+// 1
+console.log("Sentença de código");
+
+// 2
+
+{
+  {
+  }
+}
+
+// Blocos e Sentenças
+
+{
+  {
+    {
+      console.log("Olá mundo!");
+    }
+  }
+}
