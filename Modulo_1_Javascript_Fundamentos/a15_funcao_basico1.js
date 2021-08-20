974fdbb064c4eb640405212db1c4206e70cc3581
Yago <yagocarvalho.dev@gmail.com>
2021-08-20 14:32:30 -0300

    Funcao basico 1

---
+++ Modulo_1_Javascript_Fundamentos/a15_funcao_basico1.js
@@ -0,0 +1,21 @@
+// Função é uma ação, use verbos para nomeá-la.
+
+//Função sem retorno
+function imprimirSoma(a, b) {
+  console.log(a + b);
+}
+
+imprimirSoma(2, 3); //5
+imprimirSoma(2); // 2 + undefined = NaN
+imprimirSoma(2, 10, 4, 5, 6); // 12 // Vai considerar apenas o 2 primeiros parâmetros.
+imprimirSoma(); // NaN
+
+//Função com retorno
+function soma(a, b = 0) {
+  // O valor padrão de b será 0
+  return a + b;
+}
+
+console.log(soma(2, 3)); // 5
+console.log(soma(2)); // 2 + 0 = 2
+console.log(soma()); // undefined + 0 = NaN
