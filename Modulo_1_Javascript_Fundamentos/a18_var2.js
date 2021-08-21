f9bde34629d60046d04707584a61d4ea780a6c26
Yago <yagocarvalho.dev@gmail.com>
2021-08-21 18:48:04 -0300

    atualiazacao var 2

--- Modulo_1_Javascript_Fundamentos/a18_var2.js
+++ Modulo_1_Javascript_Fundamentos/a18_var2.js
@@ -9,5 +9,5 @@ console.log("fora =", numero); //2
 /*
     Uma sobrescreveu a outra pois estão no mesmo escopo (global).
     É possível ter 2 variáveis com o mesmo nome dentro de uma 
-    aplicalção desde que elas estejam em escopos diferentes.
+    aplicação desde que elas estejam em escopos diferentes.
 */
