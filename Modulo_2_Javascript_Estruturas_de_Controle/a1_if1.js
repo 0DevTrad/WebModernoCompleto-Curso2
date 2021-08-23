23d515818c1aa83b30ae1aa78a9d0a45ac6b55e4
Yago <yagocarvalho.dev@gmail.com>
2021-08-23 20:52:07 -0300

    if 1

---
+++ Modulo_2_Javascript_Estruturas_de_Controle/a1_if1.js
@@ -0,0 +1,25 @@
+function soBoaNoticia(nota) {
+  if (nota >= 7) {
+    console.log("Aprovado com " + nota);
+  }
+}
+soBoaNoticia(8.1);
+soBoaNoticia(6.1);
+
+// Usando um boolean
+function seForVerdadeEuFalo(valor) {
+  if (valor) {
+    console.log("É verdade... " + valor);
+  }
+}
+seForVerdadeEuFalo();
+seForVerdadeEuFalo(null);
+seForVerdadeEuFalo(undefined);
+seForVerdadeEuFalo("");
+seForVerdadeEuFalo(0);
+seForVerdadeEuFalo(-1); // É verdade... -1
+seForVerdadeEuFalo(" "); // É verdade...
+seForVerdadeEuFalo("?"); // É verdade... ?
+seForVerdadeEuFalo([]); // É verdade...
+seForVerdadeEuFalo([1, 2]); // É verdade... 1,2
+seForVerdadeEuFalo({}); // É verdade... [object Object]
