ecd326f4a409ad4ff185829f13b96d5ec4346f8a
Yago <yagocarvalho.dev@gmail.com>
2021-08-23 15:47:37 -0300

    operador destructuring 4

---
+++ Modulo_1_Javascript_Fundamentos/a31_destructuring3.js
@@ -0,0 +1,12 @@
+// Outro contexto para usar o destructuring é no âmbito de uma função.
+
+function rand({ min = 0, max = 1000 }) {
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
+}
+
+const obj = { max: 50, min: 40 };
+console.log(rand(obj));
+console.log(rand({ min: 955 })); // Entre 955 e 1000
+console.log(rand({})); // Entre 0 e 1000
+//console.log(rand()); // Erro!
