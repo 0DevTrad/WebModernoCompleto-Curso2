a0cc342bf3808bf10dd62f174e3587b430a2ff03
Yago <yagocarvalho.dev@gmail.com>
2021-08-23 18:31:22 -0300

    operador ternario

---
+++ Modulo_1_Javascript_Fundamentos/a37_ternario.js
@@ -0,0 +1,10 @@
+const resultado = (nota) => (nota >= 7 ? "Aprovado" : "Reprovado");
+
+console.log(resultado(7.1)); // Aprovado
+console.log(resultado(6.7)); // Reprovado
+
+/*
+    unário = 1 operandos
+    binário = 2 operandos
+    ternário = 3 operandos
+*/
