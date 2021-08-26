ed35417578039c6882c3f4a5d5ac312aa2942517
Yago <yagocarvalho.dev@gmail.com>
2021-08-26 19:36:33 -0300

    exponenciacao 2 parametros

---
+++ Exercicios_1/exercicio3.js
@@ -0,0 +1,12 @@
+/* 
+    03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao 
+    expoente.
+*/
+
+function baseElevadaAoExpoente(base, expoente) {
+    const resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(baseElevadaAoExpoente(2, 2))
