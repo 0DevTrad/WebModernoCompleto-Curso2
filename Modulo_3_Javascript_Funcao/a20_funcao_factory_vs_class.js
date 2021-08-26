027c107ee3fc123c5075b6745f8b424d5e1294f9
Yago <yagocarvalho.dev@gmail.com>
2021-08-26 14:32:11 -0300

    factory vs class

---
+++ Modulo_3_Javascript_Funcao/a20_funcao_factory_vs_class.js
@@ -0,0 +1,30 @@
+// Classe (function)
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+        //this.nome transformou a variável nome em uma variável pública
+    }
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar() // Meu nome é João.
+
+
+// Função Factory
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('Ana')
+p2.falar() // Meu nome é Ana
+
+/*
+    A função factory não necessita de que se use o this, já que
+    ela tem ciência do seu contexto léxico e onde ela foi declarada.
+*/
