a120ad569a55a368bd91d877f8dc0f82ed943972
Yago <yagocarvalho.dev@gmail.com>
2021-08-26 18:52:35 -0300

    exercicio 1

---
+++ Exercicios_1/exercicio1.js
@@ -0,0 +1,10 @@
+/* 
+    01) Crie uma função que dado dois valores (passados como parâmetros) 
+    mostre no console a soma, subtração, multiplicação e divisão desses valores.
+*/
+
+function calcular(n1, n2) {
+    console.log(n1 + n2, n1 - n2, n1 * n2, n1 / n2)
+}
+
+calcular(5, 10)
