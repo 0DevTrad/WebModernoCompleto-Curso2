779a030a857653a9ea4c5b6a477ce8e907917006
Yago <yagocarvalho.dev@gmail.com>
2021-08-26 15:08:15 -0300

    Atualização

--- Modulo_3_Javascript_Funcao/a20_funcao_factory_vs_class.js
+++ Modulo_3_Javascript_Funcao/a20_funcao_factory_vs_class.js
@@ -14,7 +14,6 @@ p1.falar() // Meu nome é João.
 
 
 // Função Factory
-
 const criarPessoa = nome => {
     return {
         falar: () => console.log(`Meu nome é ${nome}`)
@@ -25,6 +24,8 @@ const p2 = criarPessoa('Ana')
 p2.falar() // Meu nome é Ana
 
 /*
+    Não é possível instanciar uma classe sem o new.
+    
     A função factory não necessita de que se use o this, já que
     ela tem ciência do seu contexto léxico e onde ela foi declarada.
 */
