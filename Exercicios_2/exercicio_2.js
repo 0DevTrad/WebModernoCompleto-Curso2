aead2783b7f8d9eac4114edd96eef7b5f5f81817
Yago <yagocarvalho.dev@gmail.com>
2021-10-19 20:23:42 -0300

    transformar idade em anos para dias

--- Exercicios_2/exercicio_2.js
+++ Exercicios_2/exercicio_2.js
@@ -1,6 +1,15 @@
+/*
+    2) Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+
+    Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e
+    desconsidere também dias decorridos desde o último aniversário.
+*/
+
 function transformarIdadeEmDias(IdadeAnos) {
-    return IdadeAnos * 365
+    const diasDoAno = 365
+
+    return diasDoAno * IdadeAnos
 }
 
-console.log(transformarIdadeDias(25)) // 9125
-console.log(transformarIdadeDias(70)) // 25550
+console.log(transformarIdadeEmDias(25)) // 9125
+console.log(transformarIdadeEmDias(70)) // 25550
