da5d08d0771be101e2b857bbfa9416d43ea76931
Yago <yagocarvalho.dev@gmail.com>
2021-10-19 20:32:50 -0300

    calcular salario liquido

--- Exercicios_2/exercicios_3.js
+++ Exercicios_2/exercicios_3.js
@@ -1,13 +1,14 @@
 /*
     3) Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas 
-    por um funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser 
-    a string "Salário igual a R$ X", em que X é o quanto o funcionário ganhou no mês.
+    por um funcionário num mês, e o quanto ele recebe por hora. A função deverá calcular o salário 
+    líquido mensal do funcionário, que é da quantidade de horas trabalhadas no mês multiplicada 
+    pelo valor da sua hora. Desse valor, deve ser subtraído 30%, relativo a impostos.
 */
 
-// calcularSalario(150, 40.5) // retornará "Salário igual a R$ 6075"
-
-function calcularSalario(horasTrabalhadas, valorHora) {
-    return `Salário igual a R$ ${horasTrabalhadas * valorHora}`
+function calcularSalarioLiquido(horasTrabalhadas, valorHora) {
+    const salarioBruto = horasTrabalhadas * valorHora
+    const salarioLiquido = salarioBruto - (salarioBruto * 30 / 100)
+    return `Salário igual a R$ ${salarioLiquido}`
 }
 
-console.log(calcularSalario(150, 40.5))
+console.log(calcularSalarioLiquido(180, 60)) // Salário igual a R$ 7560
