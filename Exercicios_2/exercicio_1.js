e977a2597699e2da1f6b3940ed27113e4ee34332
Yago <yagocarvalho.dev@gmail.com>
2021-10-19 20:37:45 -0300

    funcao cumprimentar

--- Exercicios_2/exercicio_1.js
+++ Exercicios_2/exercicio_1.js
@@ -1,3 +1,8 @@
+/*
+    1) Crie uma função que retorna a string "Olá, " concatenada com um argumento text 
+    (a ser passado para a função) e com ponto de exclamação "!" no final.
+*/
+
 // Resolução 1
 function cumprimentar(nome) {
     return `Olá, ${nome}!`
