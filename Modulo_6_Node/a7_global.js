ff73be1d51710546c8a2cc46f45e551c24f4fd80
Yago <yagocarvalho.dev@gmail.com>
2021-10-25 03:30:49 -0300

    global

---
+++ Modulo_6_Node/a7_global.js
@@ -0,0 +1,8 @@
+global.MinhaApp = Object.freeze({
+    saudacao() {
+        return 'Estou em todos os lugares!'
+    },
+    nome: 'Sistema legal'
+})
+
+// Evitar mexer no objeto global
---
+++ Modulo_6_Node/a7_globalCliente.js
@@ -0,0 +1,6 @@
+require('./a7_global')
+
+console.log(MinhaApp.saudacao())
+
+MinhaApp.nome = 'Eita!'
+console.log(MinhaApp.nome) // Sistema legal
