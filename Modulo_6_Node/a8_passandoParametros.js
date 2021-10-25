37a669c22d8bb52ef416ee70302fc83ee39b7526
Yago <yagocarvalho.dev@gmail.com>
2021-10-25 04:13:21 -0300

    parametros entre modulos

---
+++ Modulo_6_Node/a8_passandoParametros.js
@@ -0,0 +1,5 @@
+module.exports = function (...nomes) {
+    return nomes.map(nome => `Boa semana ${nome}!`)
+}
+
+// Código interligado (Aula 8 - code 1)
---
+++ Modulo_6_Node/a8_passandoParametrosCliente.js
@@ -0,0 +1,9 @@
+const saudacoes = require('./a8_passandoParametros')('Ana', 'Lucas', 'João')
+console.log(saudacoes) // [ 'Boa semana Ana!', 'Boa semana Lucas!', 'Boa semana João!' ]
+
+// Código interligado (Aula 8 - code 2)
+
+/*
+    Como 'require('./a8_passandoParametros')' é uma função
+    é possível passar os parâmetros em seguida.
+*/
